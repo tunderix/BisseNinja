c58cf5d78ef80d65bd4d9bd575c3f9fc6a91de4b
Sampsoni <sami.repo@live.com>
2021-04-18 10:18:58 +0300

    Gatsby backend configurations Blog Posts

---
+++ src/pages/BlogPosts.tsx
@@ -0,0 +1,9 @@
+﻿import React from "react";
+import {Link} from "gatsby";
+
+export default () => {
+    return <>
+        Hello world!
+        <p><Link to="/blog">View Blog</Link></p>
+    </>
+}
