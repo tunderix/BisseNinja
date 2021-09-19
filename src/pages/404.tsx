a75cc046e36d570b5c0c055da54805b6c1537e42
Ionisaatio <sami.repo@live.com>
2021-09-19 12:48:21 +0300

    404 Fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,7 +6,7 @@ import SEO from '../components/seo';
 const NotFoundPage = () => {
   return (
     <>
-      <SEO title="404: Not found" />
+      <SEO title="404: Not found" lang="en" meta={[]} />
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </>
